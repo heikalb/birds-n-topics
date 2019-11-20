f063d5dbd8a15069c9760464bde485686e6d76ec
heikalb <heikal93@gmail.com>
2019-11-19 23:20:34 -0800

    +building topic model

---
+++ build_topic_model.py
@@ -0,0 +1,57 @@
+from get_data import get_data
+import spacy
+from nltk.stem import WordNetLemmatizer
+from gensim.models import LdaModel
+from gensim.corpora import Dictionary
+
+spacy_model = spacy.load("en_core_web_sm")
+lemmatizer = WordNetLemmatizer()
+
+
+def preprocess_documents(documents):
+    processed_docs = []
+
+    for doc in documents:
+        processed_doc = []
+        tokens = spacy_model(doc)
+
+        for token in tokens:
+            if 'NN' in token.tag_:
+                token_text = token.text.lower()
+                token_text = lemmatizer(token_text)
+                processed_doc.append(token_text)
+
+        processed_docs.append(processed_doc)
+
+    return processed_docs
+
+
+def build_model(documents):
+    dictionary = Dictionary(documents)
+    bows = [dictionary.doc2bow(doc) for doc in documents]
+    temp = dictionary[0]
+    id2word = dictionary.id2token
+
+    model = LdaModel(corpus=bows, id2word=id2word, iterations=50,
+                     num_topics=40)
+
+    return model
+
+
+def main():
+    # Get corpus
+    corpus = get_data()
+
+    # Gather all texts, regardless of taxonomic family
+    documents = [doc for fam in corpus for doc in corpus[fam]]
+
+    # Preprocess texts in corpus
+    documents = preprocess_documents(documents)
+
+    # Build and train topic model
+    topic_model = build_model(documents)
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
