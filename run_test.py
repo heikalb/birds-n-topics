bfbdb8a57dac82452081feb8f3cc6658473d7555
heikalb <heikal93@gmail.com>
2019-11-21 00:21:48 -0800

    progress

--- run_test.py
+++ run_test.py
@@ -1,9 +1,19 @@
+"""
+
+Heikal Badrulhisham, 2019 <heikal93@gmail.com>
+"""
 from build_topic_model import get_topic_model
 from build_topic_model import preprocess_documents
 from gensim.corpora import Dictionary
 
 
 def split_corpus(corpus, split_idx):
+    """
+
+    :param corpus:
+    :param split_idx:
+    :return:
+    """
     comparison_docs = dict()
     test_docs = dict()
 
@@ -14,8 +24,13 @@ def split_corpus(corpus, split_idx):
     return comparison_docs, test_docs
 
 
-
 def get_topics(corpus, topic_model):
+    """
+
+    :param corpus:
+    :param topic_model:
+    :return:
+    """
     family_topics = dict()
 
     for fam in corpus:
@@ -31,10 +46,26 @@ def get_topics(corpus, topic_model):
     return family_topics
 
 
+def compare_topics(comparison_topics, test_docs, topic_model):
+    """
+
+    :param comparison_topics:
+    :param test_docs:
+    :param topic_model:
+    :return:
+    """
+    return
+
+
 def main():
     topic_model, corpus = get_topic_model()
-    comparison_docs, test_docs
+
+    split = int(len(corpus[corpus.keys()[0]])*0.8)
+    comparison_docs, test_docs = split_corpus(corpus, split)
+
+    comparison_topics = get_topics(corpus, topic_model)
+
 
 if __name__ == '__main__':
     main()
-    exit()
+    exit(0)
